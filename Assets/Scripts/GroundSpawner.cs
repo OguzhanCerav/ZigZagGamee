f382f726841ad5af99260a5b9b056a495e370646
OguzhanCerav <116087008+OguzhanCerav@users.noreply.github.com>
2023-05-05 10:53:13 +0300

    Random Ground Spawning

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -22,6 +22,16 @@ public class GroundSpawner : MonoBehaviour
     }
     void ZeminOlustur()
     {
-        sonZemin = Instantiate(sonZemin, sonZemin.transform.position + Vector3.left, sonZemin.transform.rotation);
+        Vector3 yon;
+
+        if (Random.Range(0,2)==0)
+        {
+            yon = Vector3.left;
+        }
+        else
+        {
+            yon = Vector3.back;
+        }
+        sonZemin = Instantiate(sonZemin, sonZemin.transform.position + yon, sonZemin.transform.rotation);
     }
 }
