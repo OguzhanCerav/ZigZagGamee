72f6564afc56f187ac6a5124c86b0f4d372330c2
OguzhanCerav <116087008+OguzhanCerav@users.noreply.github.com>
2023-05-05 11:16:41 +0300

    Destroy

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -10,7 +10,7 @@ public class GroundSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 20; i++)
         {
             ZeminOlustur();
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,11 +34,15 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Zemin"))
         {
+            YokEt(collision.gameObject);
             groundSpawner.ZeminOlustur();
         }
     }
 
-
+    void YokEt(GameObject zemin)
+    {
+        Destroy(zemin);
+    }
 
 
 
